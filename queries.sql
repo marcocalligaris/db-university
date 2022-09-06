6b1166a172bec4aa6490b17327862ffd064b9f3d
marcocalligaris <marcocally@gmail.com>
2022-09-06 16:14:08 +0200

    Select query 2

--- queries.sql
+++ queries.sql
@@ -5,6 +5,8 @@ SELECT `name` AS `nome`, `surname` AS `cognome`, `date_of_birth` AS `data_di_nas
 FROM `students` WHERE YEAR(`date_of_birth`) = 1990;
 
 -- 2. Selezionare tutti i corsi che valgono più di 10 crediti (479)
+SELECT `name` AS `nome_corso`, `cfu` AS `crediti_formativi` FROM `courses` 
+WHERE `cfu` > 10;
 
 
 -- 3. Selezionare tutti gli studenti che hanno più di 30 anni
