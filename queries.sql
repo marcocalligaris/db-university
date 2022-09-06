3f55e7fbaf31d36f6bfb24161aa3448a5f257a54
marcocalligaris <marcocally@gmail.com>
2022-09-06 18:18:21 +0200

    Group by query 1

--- queries.sql
+++ queries.sql
@@ -44,7 +44,9 @@ WHERE `phone` IS NULL;
 
 
 -- 1. Contare quanti iscritti ci sono stati ogni anno
-
+SELECT COUNT(*) AS `iscritti`, YEAR(`enrolment_date`) AS `anno_iscrizione` 
+FROM `students` 
+GROUP BY `anno_iscrizione`;
 
 -- 2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
 
