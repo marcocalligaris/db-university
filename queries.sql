0399529cbf2db0b01340133cb4b5f6f16eb31b1b
marcocalligaris <marcocally@gmail.com>
2022-09-06 16:57:11 +0200

    Select query 4

--- queries.sql
+++ queries.sql
@@ -16,7 +16,9 @@ FROM `students`
 WHERE YEAR(`date_of_birth`) < 1992;
 
 -- 4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286)
-
+SELECT `name` AS `nome_corso`, `period` AS `semestre`, `year` AS `anno` 
+FROM `courses` 
+WHERE `period` = 'I semestre' AND `year` = '1';
 
 -- 5. Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020 (21)
 
