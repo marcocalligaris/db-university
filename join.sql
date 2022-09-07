8d6033320a84d3eacc90922c3d7cea1951427aee
marcocalligaris <marcocally@gmail.com>
2022-09-07 16:01:18 +0200

    Join 3

--- join.sql
+++ join.sql
@@ -19,7 +19,13 @@ ON `departments`.`id` = `degrees`.`department_id`
 WHERE `departments`.`name` LIKE 'Dipartimento di Neuroscienze';
 
 -- 3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
-
+SELECT `teachers`.`id` AS `Codice docente`, `teachers`.`name` AS `Nome`, `teachers`.`surname` AS `Cognome`, `courses`.`name` AS `Materia insegnata` 
+FROM `courses` 
+JOIN `course_teacher` 
+ON `courses`.`id` = `course_teacher`.`course_id` 
+JOIN `teachers` 
+ON `course_teacher`.`teacher_id` = `teachers`.`id` 
+WHERE `course_teacher`.`teacher_id` = 44;
 
 -- 4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il relativo dipartimento, in ordine alfabetico per cognome e nome
 
